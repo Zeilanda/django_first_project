93b6a790b0c998978fe475e082afdc044d4b09d8
Zeilanda <83118578+Zeilanda@users.noreply.github.com>
2022-09-14 23:37:21 +0300

    m2m relations

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,32 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=40, verbose_name='Раздел')
+    # articles = models.ManyToManyField(Article,
+    #                                   related_name='tags',
+    #                                   through='Scope',
+    #                                   # through_fields=('tag', 'article'),
+    #
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField()
+
+
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
