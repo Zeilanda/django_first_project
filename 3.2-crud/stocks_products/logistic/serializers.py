2c65465ad34c410bc96cb4c9df614291a55f4046
Zeilanda <dar.remez@gmail.com>
2022-09-19 22:08:19 +0300

    first part crud

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,28 +1,46 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers
+from rest_framework.filters import SearchFilter
+
+from logistic.models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'stocks']
     # настройте сериализатор для продукта
-    pass
+    # pass
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
     # настройте сериализатор для позиции продукта на складе
-    pass
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
+        # print(validated_data)
         positions = validated_data.pop('positions')
-
+        # print(positions)
         # создаем склад по его параметрам
         stock = super().create(validated_data)
 
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.create(**position)
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
@@ -31,11 +49,14 @@ class StockSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
 
+        positions = validated_data.pop('positions')
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.filter().update(**position)
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
